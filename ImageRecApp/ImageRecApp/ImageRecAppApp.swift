b213fb65c68b7e116f70b06afe35a871141485b8
kbernacik kbernacik <kbernacik@r315-c10.net.kis.agh.edu.pl>
2024-11-26 10:17:35 +0100

    Add project

---
+++ ImageRecApp/ImageRecApp/ImageRecAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRecAppApp.swift
+//  ImageRecApp
+//
+//  Created by kbernacik kbernacik on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageRecAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
